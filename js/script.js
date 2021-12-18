2c68061a68753555b8e1980a3f3567bf6a498d12
Martin Balverde <tinchoabalverde@gmail.com>
2021-12-18 19:47:08 -0300

    Se agrega funcion para que se pueda realizar el mismo pedido que se hizo la utima vez, y se arreglan otras funcionalidades

--- js/script.js
+++ js/script.js
@@ -6,17 +6,56 @@ class Alimentos {
     }
 }
 
-const produtosCafeteria = [
-    {nombre: "Cafe pequeño", precio: 90, descripcion: "Café o café con leche."},
-    {nombre: "Cafe mediano", precio: 140, descripcion: "Café o café con leche."},
-    {nombre: "Cafe grande", precio: 160, descripcion: "Café o café con leche."},
-    {nombre: "Cafe irlandes", precio: 300, descripcion: "Café con whisky."},
-    {nombre: "Submarino", precio: 340, descripcion: "Barra de chocolate dentro de una taza de leche caliente."},
-    {nombre: "Capuchino", precio: 240, descripcion: ""},
-    {nombre: "Tortitas", precio: 40, descripcion: ""},
-    {nombre: "Facturas o Medialunas", precio: 60, descripcion: ""},
-    {nombre: "Tostados", precio: 290, descripcion: ""},
-    {nombre: "Churros Churrico",precio: 80, descripcion: ""}
+const produtosCafeteria = [{
+        nombre: "Café pequeño",
+        precio: 90,
+        descripcion: "Café o café con leche."
+    },
+    {
+        nombre: "Café mediano",
+        precio: 140,
+        descripcion: "Café o café con leche."
+    },
+    {
+        nombre: "Café grande",
+        precio: 160,
+        descripcion: "Café o café con leche."
+    },
+    {
+        nombre: "Café irlandes",
+        precio: 300,
+        descripcion: "Café con whisky."
+    },
+    {
+        nombre: "Submarino",
+        precio: 340,
+        descripcion: "Barra de chocolate dentro de una taza de leche caliente."
+    },
+    {
+        nombre: "Capuchino",
+        precio: 240,
+        descripcion: ""
+    },
+    {
+        nombre: "Tortitas",
+        precio: 40,
+        descripcion: ""
+    },
+    {
+        nombre: "Facturas o Medialunas",
+        precio: 60,
+        descripcion: ""
+    },
+    {
+        nombre: "Tostados",
+        precio: 290,
+        descripcion: ""
+    },
+    {
+        nombre: "Churros Churrico",
+        precio: 80,
+        descripcion: ""
+    }
 ];
 
 // FUNCION PARA MOVER FLECHA CADA VEZ QUE SE HACE CLICK
@@ -32,30 +71,32 @@ $(".contenedor-padre").click(() => {
 
 // AGREGANDO TODOS LOS PEDIDOS AL HTML con los datos de JSON
 var cont = -1;
+var cont2 = 0;
 $("#promociones").click(function () {
-
     $.get("js/promos.json", function (promos, estado) {
         if (estado === "success") {
 
-            produtosCafeteria.push({nombre: promos[0].nombre, descripcion: promos[0].descripcion,precio: promos[0].precio});
-            produtosCafeteria.push({nombre: promos[1].nombre, descripcion:promos[1].descripcion,precio: promos[1].precio});
-            produtosCafeteria.push({nombre:promos[2].nombre, descripcion:promos[2].descripcion,precio: promos[2].precio});
-            produtosCafeteria.push({nombre:promos[3].nombre, descripcion:promos[3].descripcion,precio: promos[3].precio});
+            for (let i = 0; i < promos.length; i++) {
+                produtosCafeteria.push({ nombre: promos[i].nombre, descripcion: promos[i].descripcion, precio: promos[i].precio });
+            }
 
-            for (const promo of promos) {
-                cont += 1;
-                $("#contenedorDePromos").append(`<div id="${cont}" onClick="tomarPedido(this.id)" class="contenedorPromo">
-                                                    <h3 class="nombrePromo">${promo.nombre}</h3>
-                                                    <p class="contenidoPromo">${promo.descripcion}</p>
-                                                    <div class="flex">
-                                                        <p> </p>
-                                                        <p class="precioPromo">$${promo.precio}</p>
-                                                    </div>
-                                              </div>`);
+            if (cont2 == 0) {
+                for (const promo of promos) {
+                    cont += 1;
+                    $("#contenedorDePromos").append(`<div id="${cont}" onClick="tomarPedido(this.id)" class="contenedorPromo">
+                                            <h3 class="nombrePromo">${promo.nombre}</h3>
+                                            <p class="contenidoPromo">${promo.descripcion}</p>
+                                            <div class="flex">
+                                                <p> </p>
+                                                <p class="precioPromo">$${promo.precio}</p>
+                                            </div>
+                                      </div>`);
+                }
             }
         } else {
             Swal.fire('Hoy no hay promociones disponibles')
         }
+        cont2 += 1;
     });
 });
 
@@ -71,14 +112,22 @@ for (let i = 0; i < produtosCafeteria.length; i++) {
                                                 <p class="precios">$${produtosCafeteria[i].precio}</p>
                                             </div>`);
 }
-// TOMANDO PEDIDOS
+// SE AGREGA AL CARRITO EL PRODUCTO SELECCIONADO DE ACUERDO A SU ID Y SE LLAMA A LA FUNCION precioFinal()
 function tomarPedido(id) {
     carrito.push(produtosCafeteria[id]);
     precioFinal();
 }
+// FUNCION PARA VISUALIZAR LA CANTIDAD DE PEDIDOS EN EL CARRITO
+function numeroDePedidos() {
+    if (carrito.length > 9) {
+        $("#cantProductos").empty().append("+9");
+    } else {
+        $("#cantProductos").empty().append(carrito.length);
+    }
+}
 
-const carrito = [];
-
+var carrito = [];
+// FUNCION AGREGAR EL RESUMEN T EL PRECIO DE LOS PEDIDOS QUE SE VAN REALIZANDO
 function precioFinal() {
     let productosElegidos = "";
     var precioTotal = 0;
@@ -97,42 +146,32 @@ function precioFinal() {
                                             <p> Efectivo o débito: <br> <span> $${precioTotal} </span> </p>  <br>
                                             <p> Tarjeta de crédito(+20%): <br> <span> $${precioTotal * 1.20}</span> </p>
                                     </div>`);
-
-    $("#cantProductos").empty().append(carrito.length);
+    numeroDePedidos();
 }
 
 // FUNCION BOTON LLAMAR AL MOZO
 $("#llamar_mozo").click(function () {
-
-    
     var numMesa = $("#mesas").val();
-    
-    if ((numMesa < 26) && (numMesa > 0))  {
-    
+
+    if ((numMesa < 26) && (numMesa > 0)) {
         Swal.fire({
+            icon: 'success',
             font: "10px",
             text: 'El mozo ha sido notificado, en segundos se acercará a su mesa.'
         })
-    
     } else {
         Swal.fire({
             icon: 'question',
             font: "10px",
-            backdrop: false,
             text: 'El mozo no sabe cual es su mesa, coloque el número de mesa correspondiente.'
         })
     }
-
-
-    
 });
 // FUNCION BOTON REALIZAR PEDIDO
 $("#realizar_pedido").click(function realizarPedido() {
-
     var numMesa = $("#mesas").val();
-    
-    if ((numMesa < 26) && (numMesa > 0))  {
-        
+
+    if ((numMesa < 26) && (numMesa > 0)) {
         if (carrito.length == 0) {
             Swal.fire({
                 icon: 'error',
@@ -147,7 +186,7 @@ $("#realizar_pedido").click(function realizarPedido() {
                 title: 'Confirmar pedido',
                 text: "Una vez que confirme empezaremos a preparar su pedido!",
                 icon: 'warning',
-                iconColor:'#d33',
+                iconColor: '#d33',
                 showCancelButton: true,
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#d33',
@@ -161,16 +200,13 @@ $("#realizar_pedido").click(function realizarPedido() {
                         'Su pedido está en preparación.',
                         'success'
                     )
-                    
                     const enJSON = JSON.stringify(carrito);
                     localStorage.setItem("carrito", enJSON);
 
                     $('#muestra .listaCompras #listaDeJS').empty();
                     $('#muestra .resumen #precioDeJS').empty();
                     carrito.splice(0, carrito.length + 1);
-                    $("#cantProductos").empty().prepend(carrito.length);
-
-                    
+                    numeroDePedidos();
                 }
             })
         }
@@ -179,10 +215,7 @@ $("#realizar_pedido").click(function realizarPedido() {
             font: "10px",
             text: 'Coloque su número de mesa correctamente antes de realizar su pedido'
         })
-        
     }
-
-
 });
 // FUNCION BOTON BORRAR ULTIMO PEDIDO
 $("#borrar_pedido").click(() => {
@@ -197,9 +230,7 @@ $("#borrar_pedido").click(() => {
     } else {
         carrito.pop();
         precioFinal();
-        
     }
-
 });
 // FUNCION BOTON CANCELAR PEDIDO
 $("#cancelar_pedido").on("click", function cancelarPedido() {
@@ -214,25 +245,31 @@ $("#cancelar_pedido").on("click", function cancelarPedido() {
         carrito.splice(0, carrito.length + 1);
         $('#muestra .listaCompras #listaDeJS').empty();
         $('#muestra .resumen #precioDeJS').empty();
-        $("#cantProductos").empty().prepend(carrito.length);
+        numeroDePedidos();
     }
 });
 
 
-$("#ultimo_pedido").click(() => {
-    var ultimoPedidoRealizado = localStorage.getItem('carrito')
-    var ultimoPedido = JSON.parse(ultimoPedidoRealizado);
-
-    console.log(ultimoPedido);
-
- /*   for (let i = 0; i < ultimoPedido.length; i++) {
-        console.log(ultimoPedido[i]);
+// Si anteriormente el cliente ya ha realizado un pedido el siguiente codigo le ofrecerá realizar el mismo pedido que hizo esa vez
+$( document ).ready(function() {
+    if (localStorage.length > 0) {
+        Swal.fire ({
+            title: 'Hola de nuevo!',
+            text: 'Notamos que ya nos has visitado anteriormente lo cual nos enorgullece que hayas decidido volver. ¿Querés agregar al carrito el último pedido que realizaste la ultima vez que nos visitaste?',
+            grow: 'row',
+            cancelButtonColor: '#d33',
+            showCancelButton: true,
+            cancelButtonText: 'No me interesa',
+            confirmButtonText: 'Buena idea !',
+            backdrop: true,
+            allowOutsideClick: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                var ultimoPedidoRealizado = localStorage.getItem('carrito')
+                carrito = JSON.parse(ultimoPedidoRealizado);
+                precioFinal();
+            }
+        })
         
-    }*/
+    }
 });
-
-
-
-
-
-
